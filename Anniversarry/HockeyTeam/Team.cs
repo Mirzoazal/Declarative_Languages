8c00ede1702e7a1403762ca789b0d8287daf0e82
Mirzoazal <rmirzo5057@gmail.com>
2024-06-07 17:27:48 -0400

    made some changes in from Hockey to aniversary

---
+++ Anniversarry/HockeyTeam/Team.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * File:            Team.cs
+ * Date:            May 8, 2023
+ * Course:          INFO-3138
+ * Description:     Defines the Team and Player classes 
+ */
+
+namespace HockeyTeam
+{
+    class Player
+    {
+        public string Name { get; set; } = "";
+        public ushort Number { get; set; }
+        public string Position { get; set; } = "";
+    } // end class Player
+
+    class Team
+    {
+        public string Name { get; set; } = "";
+        public Player[] Players = Array.Empty<Player>();
+    } // end class Team
+}
---
+++ Anniversarry/HockeyTeamWithValidation/Team.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * File:            Team.cs
+ * Date:            May 8, 2023
+ * Course:          INFO-3138
+ * Description:     Defines the Team and Player classes 
+ */
+
+namespace Anniversary
+{
+    class Address
+    {
+        public string StreetAddress { get; set; } = "";
+        public string Municipality { get; set; } = "";
+        public string Province { get; set; } = "";
+        public string PostalCode { get; set; } = "";
+    } // end class Address
+
+    class Anniversary
+    {
+        public string Names { get; set; } = "";
+        public string AnniversaryDate { get; set; } = "";
+        public string AnniversaryType { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Email { get; set; } = "";
+        public Address Address { get; set; } = new Address();
+    } // end class Anniversary
+
+    class AnniversarriesArray
+    {
+        public Anniversary[] Anniversaries { get; set; } = Array.Empty<Anniversary>();
+    } // end class Team
+}
